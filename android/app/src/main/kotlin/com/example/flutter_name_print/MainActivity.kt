ad9a7fc7e3ed161a5b1b1375f71b193b08c5d81d
Muhammad Haroon <mharoon1326@gmail.com>
2024-09-26 11:14:25 +0500

    init

---
+++ android/app/src/main/kotlin/com/example/flutter_name_print/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.flutter_name_print
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
--- lib/main.dart
+++ lib/main.dart
@@ -1,32 +1,34 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           backgroundColor: Colors.blue[100],
-          title: Text(
+          title: const Text(
             'Im Haroon',
             style: TextStyle(color: Colors.black),
           ),
         ),
-        body: Center(
+        body: const Center(
           child: Text(
             'My App',
             style: TextStyle(fontSize: 24),
           ),
         ),
-        bottomNavigationBar: BottomAppBar(
+        bottomNavigationBar: const BottomAppBar(
           child: Row(
             mainAxisAlignment: MainAxisAlignment.spaceAround,
             children: [
-              IconButton(icon: Icon(Icons.arrow_back), onPressed: () {}),
-              IconButton(icon: Icon(Icons.circle_outlined), onPressed: () {}),
-              IconButton(icon: Icon(Icons.menu), onPressed: () {}),
+              IconButton(icon: Icon(Icons.arrow_back), onPressed: null),
+              IconButton(icon: Icon(Icons.circle_outlined), onPressed: null),
+              IconButton(icon: Icon(Icons.menu), onPressed: null),
             ],
           ),
         ),
